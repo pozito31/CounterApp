764ac873a0c74904b9c3086d80e5a183e2ddacc0
pozito31 <jessicapozo198715@gmail.com>
2022-09-08 09:38:14 +0200

    subir actualizacion

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <CounterApp value={ 10 } />
+        <CounterApp value={ 0 } />
     </React.StrictMode>
 );
